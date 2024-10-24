08e6ceee9d2a30263cd8e07efaba5aefcddfef2a
skidy89 <tutorialeyg310@gmail.com>
2024-10-23 23:58:06 -0600

    first init

---
+++ installer.sh
@@ -0,0 +1,47 @@
+echo  -e " 
+ ___  _ __   ___ _ __  
+ / _ \| '_ \ / _ \ '_ \ 
+| (_) | |_) |  __/ | | |
+ \___/| .__/ \___|_| |_|
+      |_|               
+
+open source project installer
+"
+
+echo "Instalando pnpm..."
+
+if ! command -v pnpm &> /dev/null
+then
+    echo "[ ! ] pnpm is not installed, trying installation"
+
+    if ! command -v node &> /dev/null
+    then
+        echo "[ X ] nodejs is not installed. please install nodejs first"
+        exit 1
+    fi
+
+    npm install -g pnpm
+
+    if command -v pnpm &> /dev/null
+    then
+        echo "[✅] pnpm installed successfully"
+    else
+        echo "[X] Failed to install pnpm."
+        exit 1
+    fi
+else
+    echo '[✅] pnpm already installed'
+fi
+# Import dependencies
+echo "Importing dependencies..."
+pnpm import
+
+# Install dependencies
+echo "Installing dependencies..."
+pnpm install
+
+
+
+echo "Script finished. Press any key to exit..."
+read -n 1 -s -r -p ""
+exit 0
